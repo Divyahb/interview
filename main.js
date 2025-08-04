ca2eaae8989ae3b09334c6baec77867e2f913723
Divya H B <divyalogan92@gmail.com>
2025-08-04 15:47:20 +0530

    Fixed issues in main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(3000, function() {
